bb51e25b6ef3ea6a875be18aed8df5f111868834
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-06-03 10:53:45 +0300

    Add winston and morgan loggers

--- node-express-website/server.js
+++ node-express-website/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const logger = require('./services/logger');
 
 const app = express();
 const port = 3000;
 
+app.use(morgan('short', { stream: logger.stream }));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => logger.info(`Example app listening on port port!`));
