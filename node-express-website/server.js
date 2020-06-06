c02a700b7a9b709efcd2ab40a25f16378790efe8
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-06 10:47:44 +0300

    Add express static content serving functionality

--- node-express-website/server.js
+++ node-express-website/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const logger = require('./services/logger');
 
@@ -8,6 +9,14 @@ const port = 3000;
 
 app.use(morgan('short', { stream: logger.stream }));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
 
 app.listen(port, () => logger.info('Example app listening on port port!'));
