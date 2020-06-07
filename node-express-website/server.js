db99c042a5513ba52a2c39fd0947fb234983b7f5
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-07 12:17:00 +0300

    Add services functionality

--- node-express-website/server.js
+++ node-express-website/server.js
@@ -3,7 +3,12 @@ const morgan = require('morgan');
 const path = require('path');
 
 const logger = require('./services/logger');
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakersService');
 const routes = require('./routes');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
 
 const app = express();
 const port = 3000;
@@ -15,6 +20,12 @@ app.use(morgan('short', { stream: logger.stream }));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  }),
+);
 
 app.listen(port, () => logger.info('Example app listening on port port!'));
