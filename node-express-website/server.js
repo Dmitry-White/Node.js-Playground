204e27af2ca9044c42d63d8e7dbb5a30a726234f
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-07 11:46:38 +0300

    Add Router middleware and index route

--- node-express-website/server.js
+++ node-express-website/server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const logger = require('./services/logger');
+const routes = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -14,12 +15,6 @@ app.use(morgan('short', { stream: logger.stream }));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use('/', routes());
 
 app.listen(port, () => logger.info('Example app listening on port port!'));
