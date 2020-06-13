ca219485f09bdf867608499aa92bb9ea14e16381
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-13 15:32:10 +0300

    Add try-catch decorator

---
+++ node-express-website/decorators/withError.js
@@ -0,0 +1,12 @@
+const withError = (handler) => {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res)
+      return next();
+    } catch(error) {
+      return next(error);
+    }
+  }
+}
+
+module.exports = withError;
--- node-express-website/routes/feedback.js
+++ node-express-website/routes/feedback.js
@@ -1,18 +1,20 @@
 const express = require('express');
 
+const withError = require('../decorators/withError');
+
 const router = express.Router();
 
 const feedbackRoute = (params) => {
   const { feedbackService } = params;
 
-  router.get('/', async (req, res) => {
+  router.get('/', withError(async (req, res) => {
     const feedbacks = await feedbackService.getList();
     return res.json(feedbacks);
-  });
+  }));
 
-  router.post('/', (req, res) => {
+  router.post('/', withError((req, res) => {
     return res.send('Feedback form posted');
-  });
+  }));
 
   return router;
 };
