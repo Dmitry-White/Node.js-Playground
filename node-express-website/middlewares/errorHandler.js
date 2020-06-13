147d8f49b281f8eff24a617733d7db42a067665e
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-13 16:12:17 +0300

    Add error page

--- node-express-website/middlewares/errorHandler.js
+++ node-express-website/middlewares/errorHandler.js
@@ -1,6 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(500).send('Error page!');;
-  // res.status(500).render('error', { error: err });
+  // res.status(500).send('Error page!');
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.status(status).render('error', { message: err.message, status });
 }
 
 module.exports = errorHandler;
--- node-express-website/middlewares/logErrors.js
+++ node-express-website/middlewares/logErrors.js
@@ -2,6 +2,7 @@ const logger = require('../services/logger');
 
 const logErrors = (err, req, res, next) => {
   logger.error({
+    status: err.status || 500,
     message: err.message,
     stack: err.stack
   });
