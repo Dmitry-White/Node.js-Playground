752fdd086988d89bd9c5120775e98199a3654e52
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-16 10:51:50 +0300

    Add mongo client

--- databases/maxcoin/test.js
+++ databases/maxcoin/test.js
@@ -1,5 +1,18 @@
 // request is a module that makes http calls easier
 const request = require('request');
+const { MongoClient } = require('mongodb');
+
+// A DSN is the name for a pointer that is used by a client application to find and connect to a database.
+// A connection string is a string that specifies information about a data source and the means of connecting to it.
+// It is passed in code to an underlying driver or provider in order to initiate the connection.
+// Whilst commonly used for a database connection, the data source could also be a spreadsheet or text file.
+const DSN = 'mongodb://localhost:37017/maxcoin';
+
+MongoClient.connect(DSN, (err, db) => {
+    if (err) throw err;
+    console.log('Connected successfully to MongoDB server');
+    db.close();
+})
 
 // Generic function that fetches the closing bitcoin dates of the last month from a public API
 function fetchFromAPI(callback) {
