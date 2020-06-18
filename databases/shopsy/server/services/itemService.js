6ac4d44381fae1a09546aef4a80704fedc80e48d
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-18 22:28:23 +0300

    Add create, update, delete operation for items

--- databases/shopsy/server/services/itemService.js
+++ databases/shopsy/server/services/itemService.js
@@ -1,7 +1,8 @@
 const ItemModel = require('../models/mongoose/Item');
 
 async function create(data) {
-
+  const item = new ItemModel(data);
+  return item.save();
 }
 
 async function getAll() {
@@ -14,11 +15,20 @@ async function getOne(itemId) {
 }
 
 async function update(itemId, data) {
+  const item = await getOne(itemId);
 
+  if (!item) throw new Error('Could not find the requested item');
+
+  Object.entries(data).forEach(([key, value]) => {
+    item[key] = value;
+  });
+
+  return item.save();
 }
 
-async function remove(itemId) {
-
+async function remove(query) {
+  const result = await ItemModel.remove(query);
+  return result.n;
 }
 
 module.exports = {
