0d9de8e26ebaa45ba9d277940305476182b84375
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-06-21 23:12:58 +0300

    Add coin funcition data to maxcoin

--- databases/maxcoin/test.js
+++ databases/maxcoin/test.js
@@ -110,13 +110,12 @@ const connection = mysql.createConnection({
 });
 
 const insertMysql = (connection, data, callback) => {
-    const values = ['values'];
+    const values = [];
 
     const sql = 'INSERT INTO coinvalues (valuedate, coinvalue) VALUES ?';
 
     Object.entries(data).forEach(([key, value]) => {
-        values.push(value);
-        values.push(key);
+        values.push([key, value]);
     });
 
     connection.query(sql, [values], callback);
@@ -126,6 +125,22 @@ connection.connect((err) => {
     if (err) throw err;
     console.time('MySQL');
     console.log('Connected successfully to MySQL server');
-    console.timeEnd('MySQL');
-    connection.end();
+
+    fetchFromAPI((err, data) => {
+        if (err) throw err;
+
+        insertMysql(connection, data.bpi, (err, results) => {
+            if (err) throw err;
+            console.log(`Successfully inserted ${results.affectedRows} documents into MySQL`);
+
+            const sql = 'SELECT * FROM coinvalues ORDER BY coinvalue DESC LIMIT 0,1';
+
+            connection.query(sql, (err, result) => {
+                if (err) throw err;
+                console.log(`MySQL: The one month max value is ${result[0].coinvalue} and it was reached on ${result[0].valuedate}`);
+                console.timeEnd('MySQL');
+                connection.end();
+            });
+        });
+    });
 })
