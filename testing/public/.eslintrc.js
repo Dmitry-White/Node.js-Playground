c351d2ab028d0b54c9441e601f4b2371d9c37009
Dmitry White <dmitry.b.white@gmail.com>
2020-06-25 18:39:34 +0100

    Add .eslintrc and .eslintignore

---
+++ testing/public/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parserOptions: {
+    sourceType: "script",
+  },
+  env: {
+    browser: true,
+    jquery: true,
+    node: false,
+  },
+  rules: {
+    "no-var": "off",
+    "prefer-const": "off",
+  },
+};
