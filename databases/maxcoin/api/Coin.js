c42dada5944f0693a58c7a56a3567498620eb531
Dmitry White <dmitry.b.white@gmail.com>
2020-07-07 15:14:56 +0100

    Refactor database repos: redis service

--- databases/maxcoin/api/Coin.js
+++ databases/maxcoin/api/Coin.js
@@ -5,31 +5,31 @@ class CoinAPI {
     this.API_URL = 'https://api.coindesk.com/v1/bpi/historical/close.json';
   }
 
-  formatDate(date) {
+  static formatDate(date) {
     const dateObj = new Date(date);
     const year = dateObj.getFullYear();
-    let month = '' + (dateObj.getMonth() + 1);
-    let day = '' + dateObj.getDate();
+    let month = `${dateObj.getMonth() + 1}`;
+    let day = `${dateObj.getDate()}`;
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+    if (month.length < 2) month = `0${month}`;
+    if (day.length < 2) day = `0${day}`;
 
     return [year, month, day].join('-');
   }
 
-  getRange(years) {
+  static getRange(years) {
     const today = new Date();
-    const end = this.formatDate(today);
+    const end = CoinAPI.formatDate(today);
 
     const pastDate = today.setFullYear(today.getFullYear() - years);
-    const start = this.formatDate(pastDate);
+    const start = CoinAPI.formatDate(pastDate);
 
     return { start, end };
   }
 
   // Generic function that fetches the closing bitcoin dates of the last month from a public API
   async fetch() {
-    const { start, end } = this.getRange(5);
+    const { start, end } = CoinAPI.getRange(5);
 
     const url = `${this.API_URL}?start=${start}&end=${end}`;
 
--- databases/maxcoin/services/Redis.js
+++ databases/maxcoin/services/Redis.js
@@ -4,53 +4,101 @@ const CoinAPI = require('../api/Coin');
 
 const REDIS_PORT = 7379;
 
+// A data source name (DSN) is the name for a pointer that is used by a client application to find and connect to a database.
+// A connection string is a string that specifies information about a data source and the means of connecting to it.
+// It is passed in code to an underlying driver or provider in order to initiate the connection.
+// Whilst commonly used for a database connection, the data source could also be a spreadsheet or text file.
+const DSN = `redis://localhost:${REDIS_PORT}`;
+
 class RedisService {
   constructor() {
     this.coinAPI = new CoinAPI();
+
+    this.client = null;
   }
 
   async connect() {
-    const RedisClient = redis.createClient(REDIS_PORT);
-
-    RedisClient.on('connect', () => {
-      console.time('Redis');
-      console.log('Connected successfully to Redis server');
-
-      fetchFromAPI((err, data) => {
-        if (err) throw err;
-
-        insertRedis(RedisClient, data.bpi, (err, results) => {
-          if (err) throw err;
-          console.log(
-            `Successfully inserted ${results} key/value pairs into Redis`,
-          );
-
-          RedisClient.zrange('values', -1, -1, 'withscores', (err, result) => {
-            if (err) throw err;
-            console.log(
-              `Redis: The one month max value is ${result[1]} and it was reached on ${result[0]}`,
-            );
-            console.timeEnd('Redis');
-            RedisClient.end();
-          });
-        });
-      });
+    this.client = redis.createClient({
+      url: DSN,
     });
+
+    await this.client.connect();
+
+    return this.client;
   }
 
-  async disconnect() {}
+  disconnect() {
+    if (!this.client) {
+      return false;
+    }
+
+    return this.client.disconnect();
+  }
 
   async insert() {
-    const insertRedis = (client, data, callback) => {
-      const values = ['values'];
+    const data = await this.coinAPI.fetch();
+    const values = [];
 
-      Object.entries(data).forEach(([key, value]) => {
-        values.push(value);
-        values.push(key);
-      });
+    Object.entries(data.bpi).forEach(([key, value]) => {
+      const dataInsert = {
+        score: value,
+        value: key,
+      };
+      values.push(dataInsert);
+    });
 
-      client.zadd(values, callback);
-    };
+    return this.client.zAdd('maxcoin:values', values);
+  }
+
+  getMax() {
+    const min = -1;
+    const max = -1;
+    const options = 'WITHSCORES';
+    return this.client.zRange('maxcoin:values', min, max, options);
+  }
+
+  async max() {
+    try {
+      console.time('Redis Connect');
+      console.info('Connecting to Redis...');
+
+      await this.connect();
+
+      console.info('Connected successfully to Redis server');
+      console.timeEnd('Redis Connect');
+
+      console.time('Redis Insert');
+      console.info('Inserting into Redis...');
+
+      const insertResult = await this.insert();
+
+      console.info(
+        `Successfully inserted ${insertResult} key/value pairs into Redis`,
+      );
+      console.timeEnd('Redis Insert');
+
+      console.time('Redis Find');
+      console.info('Querying Redis...');
+
+      const queryResult = await this.getMax();
+
+      console.log(
+        `Redis: The five year max value is ${queryResult.value} and it was reached on ${queryResult.date}`,
+      );
+      console.timeEnd('Redis Find');
+
+      console.time('Redis Disconnect');
+      console.info('Disconnecting from Redis...');
+
+      await this.disconnect();
+
+      console.info('Disconnected successfully from Redis server');
+      console.timeEnd('Redis Disconnect');
+
+      return queryResult;
+    } catch (error) {
+      throw new Error('Something went wrong.', error);
+    }
   }
 }
 
