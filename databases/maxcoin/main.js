c1d62ecdebb7fb12fbbbee0a65fa36009ce578a5
Dmitry White <dmitry.b.white@gmail.com>
2020-07-07 15:14:48 +0100

    Refactor database repos: redis setup

--- databases/maxcoin/main.js
+++ databases/maxcoin/main.js
@@ -1,9 +1,17 @@
 const MongoService = require('./services/MongoDB');
+const RedisService = require('./services/Redis');
 
-async function run() {
+async function runMongo() {
   const mongoService = new MongoService();
   return mongoService.max();
 }
+
+async function runRedis() {
+  const redisService = new RedisService();
+  return redisService.max();
+}
+
+const run = runRedis;
 
 run()
   .then((result) => console.log(result))
