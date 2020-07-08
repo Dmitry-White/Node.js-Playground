0c07fcbadfea3483ef74fc882cafa50b48333e5f
Dmitry White <dmitry.b.white@gmail.com>
2020-07-08 23:15:16 +0100

    Refactor database repos: mysql service

--- databases/maxcoin/services/MySQL.js
+++ databases/maxcoin/services/MySQL.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const CoinAPI = require('../api/Coin');
 
@@ -7,16 +7,19 @@ const MYSQL_PORT = 3406;
 class MySQLService {
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.connection = null;
   }
 
   async connect() {
-    const connection = mysql.createConnection({
+    this.connection = await mysql.createConnection({
       host: 'localhost',
       port: MYSQL_PORT,
       user: 'root',
-      password: 'mypassword',
+      password: 'qwerty',
       database: 'maxcoin',
     });
+
+    return this.connection;
 
     connection.connect((err) => {
       if (err) throw err;
@@ -48,20 +51,75 @@ class MySQLService {
     });
   }
 
-  async disconnect() {}
+  disconnect() {
+    if (!this.connection) {
+      return false;
+    }
+
+    return this.connection.end();
+  }
 
   async insert() {
-    const insertMysql = (connection, data, callback) => {
-      const values = [];
+    const data = await this.coinAPI.fetch();
+    const values = [];
 
-      const sql = 'INSERT INTO coinvalues (valuedate, coinvalue) VALUES ?';
+    Object.entries(data.bpi).forEach(([key, value]) => {
+      values.push([key, value]);
+    });
 
-      Object.entries(data).forEach(([key, value]) => {
-        values.push([key, value]);
-      });
+    const sql = 'INSERT INTO coinvalues (valuedate, coinvalue) VALUES ?';
 
-      connection.query(sql, [values], callback);
-    };
+    return this.connection.query(sql, [values]);
+  }
+
+  getMax() {
+    const sql = 'SELECT * FROM coinvalues ORDER BY coinvalue DESC LIMIT 0,1';
+    return this.connection.query(sql);
+  }
+
+  async max() {
+    try {
+      console.time('MySQL Connect');
+      console.info('Connecting to MySQL...');
+
+      await this.connect();
+
+      console.info('Connected successfully to MySQL server');
+      console.timeEnd('MySQL Connect');
+
+      console.time('MySQL Insert');
+      console.info('Inserting into MySQL...');
+
+      const insertResult = await this.insert();
+
+      console.info(
+        `Successfully inserted ${insertResult[0].affectedRows} rows into MySQL`,
+      );
+      console.timeEnd('MySQL Insert');
+
+      console.time('MySQL Find');
+      console.info('Querying MySQL...');
+
+      const queryResult = await this.getMax();
+      const row = queryResult[0][0];
+
+      console.log(
+        `MySQL: The five year max value is ${row.coinvalue} and it was reached on ${row.valuedate}`,
+      );
+      console.timeEnd('MySQL Find');
+
+      console.time('MySQL Disconnect');
+      console.info('Disconnecting from MySQL...');
+
+      await this.disconnect();
+
+      console.info('Disconnected successfully from MySQL server');
+      console.timeEnd('MySQL Disconnect');
+
+      return row;
+    } catch (error) {
+      throw new Error('Something went wrong.', error);
+    }
   }
 }
 
