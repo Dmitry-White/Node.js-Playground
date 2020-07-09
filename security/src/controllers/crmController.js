aab2c39d0ddf6dbea33adadcb58af46f8e11dd39
Dmitry White <dmitry.b.white@gmail.com>
2020-07-09 15:04:48 +0100

    Security: initial setup

---
+++ security/src/controllers/crmController.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+
+import { ContactSchema } from '../models/crmModel';
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+const addNewContact = (req, res) => {
+  const newContact = new Contact(req.body);
+
+  newContact.save((err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
+
+const getContacts = (req, res) => {
+  Contact.find({}, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
+
+const getContactWithID = (req, res) => {
+  Contact.findById(req.params.contactId, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
+
+const updateContact = (req, res) => {
+  Contact.findOneAndUpdate(
+    { _id: req.params.contactId },
+    req.body,
+    { new: true },
+    (err, contact) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(contact);
+    },
+  );
+};
+
+const deleteContact = (req, res) => {
+  Contact.remove({ _id: req.params.contactId }, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    console.log(contact);
+    res.json({ message: 'Successfully deleted contact' });
+  });
+};
+
+export {
+  addNewContact,
+  getContacts,
+  getContactWithID,
+  updateContact,
+  deleteContact,
+};
