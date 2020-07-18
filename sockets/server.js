3ec043f36a74de981976fc4611ee0945cec2d99d
Dmitry White <dmitry.b.white@gmail.com>
2020-07-18 02:44:25 +0100

    Sockets: add socket.io setup

---
+++ sockets/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import { Server } from 'socket.io';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+const server = app.listen(PORT, () =>
+  console.log('[app] Your server is running on port: ', PORT),
+);
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('[io server] User connected!');
+  socket.emit('message', { manny: 'hey, how are you?' });
+  socket.on('another event', (data) => {
+    console.log('[io server] another event: ', data);
+  });
+});
