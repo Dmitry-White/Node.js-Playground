69dce41d88147de999a2dba18af939b84725add5
Dmitry White <dmitry.b.white@gmail.com>
2020-07-28 22:21:18 +0100

    Sockets: add multiple rooms support

--- sockets/public/client.js
+++ sockets/public/client.js
@@ -10,16 +10,18 @@ const NAMESPACES = {
 };
 
 const ROOMS = {
-  JAVASCRIPT: 'javascript',
+  JAVASCRIPT: 'js',
   REACT: 'react',
+  CSS: 'css',
 };
 
-const messageForm = document.querySelector('#message_form');
-const messageInput = document.querySelector('#message_input');
-const messageHistory = document.querySelector('#messages_history');
+const messagesContainerTemplate = document.querySelector('#messages_container');
 const incomingMessageTemplate = document.querySelector('#incoming_message');
 const outgoingMessageTemplate = document.querySelector('#outgoing_message');
+const chatRoomTemplate = document.querySelector('#chat_room');
 const connectionToast = document.querySelector('#connection-toast');
+const chatInbox = document.querySelector('.chat_inbox');
+const chatRooms = document.querySelector('.chat_rooms');
 
 const socket = io(NAMESPACES.USERS); // eslint-disable-line
 
@@ -44,6 +46,25 @@ const formatTime = (timestamp) => {
   } ${dateObj.getDate()} ${dateObj.getFullYear()}`;
 };
 
+const formatDate = (timestamp) => {
+  const MONTHS = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const dateObj = new Date(timestamp);
+  return `${MONTHS[dateObj.getMonth()]} ${dateObj.getDate()}`;
+};
+
 const showConnectionToast = () => {
   console.log('[io client] connect: ', true);
 
@@ -51,8 +72,24 @@ const showConnectionToast = () => {
   toast.show();
 };
 
+const updateChatRooms = (message) => {
+  if (!message.data.room) return;
+
+  const targetRoom = Array.from(chatRooms.children).find(
+    (room) => room.id === message.data.room,
+  );
+
+  const chatDate = targetRoom.querySelector('.chat_date');
+  chatDate.textContent = formatDate(message.timestamp);
+
+  const chatText = targetRoom.querySelector('.chat_text');
+  chatText.textContent = message.data.value;
+};
+
 const updateMessageHistory = (message) => {
   console.log('[io client] message: ', message);
+  const messageHistory = chatInbox.querySelector('#messages_history');
+  if (!messageHistory) return;
 
   const messageTemplate =
     socket.id === message.id
@@ -90,20 +127,69 @@ const sendJoin = (room) => {
   socket.emit(EVENTS.JOIN, message);
 };
 
-messageForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+const renderMessageHistory = (room) => {
+  const oldMessagesContainer = chatInbox.querySelector('.messages_container');
+  if (oldMessagesContainer) chatInbox.removeChild(oldMessagesContainer);
 
-  sendMessage(ROOMS.JAVASCRIPT, messageInput.value);
+  const messagesContainerClone =
+    messagesContainerTemplate.content.cloneNode(true);
 
-  messageInput.value = '';
-});
+  const messagesContainer = messagesContainerClone.querySelector(
+    '.messages_container',
+  );
+  messagesContainer.id = room;
 
-const chatLogic = () => {
-  socket.on(EVENTS.CONNECT, () => {
-    showConnectionToast();
-    sendJoin(ROOMS.JAVASCRIPT);
+  const messageForm = messagesContainerClone.querySelector('#message_form');
+  const messageInput = messagesContainerClone.querySelector('#message_input');
+
+  messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    sendMessage(room, messageInput.value);
+
+    messageInput.value = '';
   });
-  socket.on(EVENTS.MESSAGE, updateMessageHistory);
+
+  chatInbox.append(messagesContainerClone);
 };
 
+const renderChatRooms = () => {
+  Object.entries(ROOMS).forEach(([key, value]) => {
+    const chatRoomClone = chatRoomTemplate.content.cloneNode(true);
+    const chatRoom = chatRoomClone.querySelector('.chat_room');
+    const chatRoomName = chatRoomClone.querySelector('.chat_name');
+    const chatRoomImage = chatRoomClone.querySelector('.chat_img');
+    chatRoom.id = value;
+    chatRoomName.textContent = key;
+    chatRoomImage.alt = value;
+    chatRoomImage.src = `https://img.icons8.com/small/16/000000/${value}.png`;
+    chatRooms.append(chatRoomClone);
+  });
+};
+
+const chatLogic = () => {
+  socket.on(EVENTS.CONNECT, showConnectionToast);
+  socket.on(EVENTS.MESSAGE, (message) => {
+    updateMessageHistory(message);
+    updateChatRooms(message);
+  });
+};
+
+chatRooms.addEventListener('click', (e) => {
+  if (!e.target.closest('.chat_room')) {
+    return;
+  }
+
+  Array.from(chatRooms.children).forEach((room) => {
+    room.classList.remove('active_chat');
+  });
+
+  const selectedRoom = e.target.closest('.chat_room');
+  selectedRoom.classList.add('active_chat');
+
+  renderMessageHistory(selectedRoom.id);
+  sendJoin(selectedRoom.id);
+});
+
 chatLogic();
+renderChatRooms();
